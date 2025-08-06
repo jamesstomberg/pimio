691cfe3b6e0a66caeb1681a4b5a4356d9a11830b
James Stomberg <james@vistrom.se>
2025-08-06 23:34:54 +0200

    Add page for handling users.

---
+++ src/app/user/page.tsx
@@ -0,0 +1,27 @@
+import CreateUser from '../_components/CreateUser';
+import { getSession } from '../_lib/auth';
+
+export default async function User() {
+    const session = await getSession();
+
+    // Check for token validity in the session.
+    if (!session || !session.user || !session.user.token) {
+        console.error('Invalid session or missing token.');
+        return (
+            <section>
+                <h1>Användare</h1>
+                <p>Authentication failed. Please log in again.</p>
+            </section>
+        );
+    }
+
+    const token = session.user.token;
+
+    return (
+        <section>
+            <h1>Lägg till ny användare</h1>
+
+            <CreateUser token={token} />
+        </section>
+    );
+}
