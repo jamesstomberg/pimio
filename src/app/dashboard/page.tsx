bec90a8ea63b59726f7033fa0d2ffb567fa2caa0
James Stomberg <james@vistrom.se>
2025-08-06 11:09:57 +0200

    Add page for dashboard view.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default async function Dashboard() {
+    return (
+        <section>
+            <h1>Dashboard</h1>
+            <ul>
+                <li>
+                    <Link href="/logout">Logga ut</Link>
+                </li>
+                <li>
+                    <Link href="/product">Produkter</Link>
+                </li>
+            </ul>
+        </section>
+    );
+}
